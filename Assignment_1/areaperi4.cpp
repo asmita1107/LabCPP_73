bdd7d44821070e8eb8a64cf3b68f53c9fc8aa1a3
asmita1107 <asmitaj628@gmail.com>
2026-08-06 16:45:08 +0000

    assignment1

---
+++ Assignment_1/areaperi4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float length,breadth,area,perimeter;
+
+    cout<<"Enter length and breadth:";
+    cin>>length>>breadth;
+
+    area=length*breadth;
+    perimeter=2*length*breadth;
+
+    cout<<"Area="<<area<<endl;
+    cout<<"Perimeter="<<perimeter<<endl;
+
+    return 0;
+}
