60a81a880dfff7ce9378985f804ca9ace4905553
asmita1107 <asmitaj628@gmail.com>
2026-08-23 13:24:41 +0530

    classobject

--- Class&Object/timeclass1.cpp
+++ Class&Object/timeclass1.cpp
@@ -20,8 +20,11 @@ public:
     return t3;
 }
 
- Time subtract(const Time &t) {
-  return Time(hour-t.hour, minute-t.minute);
+ Time subtract(Time t1, Time t2) {
+  Time t3;
+  t3.hour = t1.hour - t2.hour;
+  t3.minute = t1.minute - t2.minute;
+  return t3;
 }
 
  void display() const {
@@ -33,7 +36,7 @@ int main() {
  Time t1(5,40), t2(2,30);
 
  Time sum = t1.add(t1,t2);
- Time diff = t1.subtract(t2);
+ Time diff = t1.subtract(t1,t2);
 
  cout << "First Time: "; t1.display();
  cout << "Second Time: "; t2.display();
