2a829443ca961b3fa02dd90391427c05934547e6
asmita1107 <asmitaj628@gmail.com>
2026-08-23 15:36:45 +0530

    classobject

--- Class&Object/fractionclass1.cpp
+++ Class&Object/fractionclass1.cpp
@@ -8,18 +8,18 @@ private:
 public:
  Fraction(int n = 0, int d = 1) : numerator(n), denominator(d) {}
 
- Fraction add(const Fraction &f) {
+ Fraction add(Fraction f1,Fraction f2) {
   Fraction f3;
-  f3.numerator = numerator * f.denominator + f.numerator * denominator;
-  f3.denominator = denominator * f.denominator;
+  f3.numerator = f1.numerator * f2.denominator + f2.numerator * f1.denominator;
+  f3.denominator = f1.denominator * f2.denominator;
 
   return f3;
 }
 
- Fraction subtract(const Fraction &f) {
+ Fraction subtract(Fraction f1,Fraction f2) {
   Fraction f3;
-  f3.numerator = numerator * f.denominator - f.numerator * denominator;
-  f3.denominator = denominator * f.denominator;
+  f3.numerator = f1.numerator * f2.denominator - f2.numerator * f1.denominator;
+  f3.denominator = f1.denominator * f2.denominator;
 
   return f3;
 }
@@ -33,8 +33,8 @@ int main() {
  Fraction f1(3, 4);
  Fraction f2(1, 2);
 
- Fraction sum = f1.add(f2);
- Fraction diff = f1.subtract(f2);
+ Fraction sum = f1.add(f1,f2);
+ Fraction diff = f1.subtract(f1,f2);
 
  cout << "First Fraction: ";
  f1.display();
