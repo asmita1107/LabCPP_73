45249c7a9c7cca9b5772a8d2077b272a2f57bef9
asmita1107 <asmitaj628@gmail.com>
2026-08-23 13:40:14 +0530

    classobject

---
+++ Class&Object/fractionclass1.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class Fraction {
+private:
+ int numerator, denominator;
+
+public:
+ Fraction(int n = 0, int d = 1) : numerator(n), denominator(d) {}
+
+ Fraction add(const Fraction &f) {
+  Fraction f3;
+  f3.numerator = numerator * f.denominator + f.numerator * denominator;
+  f3.denominator = denominator * f.denominator;
+
+  return f3;
+}
+
+ Fraction subtract(const Fraction &f) {
+  Fraction f3;
+  f3.numerator = numerator * f.denominator - f.numerator * denominator;
+  f3.denominator = denominator * f.denominator;
+
+  return f3;
+}
+
+ void display() const {
+  cout << numerator << "/" << denominator << endl;
+}
+};
+
+int main() {
+ Fraction f1(3, 4);
+ Fraction f2(1, 2);
+
+ Fraction sum = f1.add(f2);
+ Fraction diff = f1.subtract(f2);
+
+ cout << "First Fraction: ";
+ f1.display();
+
+ cout << "Second Fraction: ";
+ f2.display();
+
+ cout << "Addition: ";
+ sum.display();
+
+ cout << "Subtraction: ";
+ diff.display();
+
+ return 0;
+}
